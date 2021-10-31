a27eed579136adebeda1791011c7eb8f30656c8f
ZiF1R <do-alex03@mail.ru>
2021-10-31 19:36:45 +0300

    Finish version for dist

--- precache-manifest.27e809a0dd2d549a164252abf556ac2d.js
+++ precache-manifest.27e809a0dd2d549a164252abf556ac2d.js
@@ -4,23 +4,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flash-cards-new/brain.svg"
   },
   {
-    "revision": "657279c467441f88b53a",
-    "url": "/flash-cards-new/css/app.b064736b.css"
+    "revision": "d50c56c377d09fc921c8",
+    "url": "/flash-cards-new/css/app.5160372c.css"
   },
   {
-    "revision": "cd006f61f4dcc4d77721",
-    "url": "/flash-cards-new/css/foldercontent.5248d234.css"
+    "revision": "b0d8749f5d1b548a1254",
+    "url": "/flash-cards-new/css/foldercontent.4352c070.css"
   },
   {
-    "revision": "e1da74122d2911c153ce",
-    "url": "/flash-cards-new/css/folders.3dd1bbd1.css"
+    "revision": "c3bf966eee5c601de047",
+    "url": "/flash-cards-new/css/folders.dbab0dc8.css"
   },
   {
     "revision": "6a797cf503f1bd7b9e18",
     "url": "/flash-cards-new/css/login.20033771.css"
   },
   {
-    "revision": "ef49853c16bc60c3d468",
+    "revision": "6707bc034a75c8783e28",
     "url": "/flash-cards-new/css/profile.eb3d7559.css"
   },
   {
@@ -28,12 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flash-cards-new/css/registration.20033771.css"
   },
   {
-    "revision": "32ce3d8408a73294a2ed",
-    "url": "/flash-cards-new/css/settings.4a24a48f.css"
-  },
-  {
-    "revision": "ba7517595d44de36bca9379992f2cd22",
-    "url": "/flash-cards-new/img/CloseTab.ba751759.svg"
+    "revision": "d9bdc5fa191d088b58a0",
+    "url": "/flash-cards-new/css/settings.b13ffd26.css"
   },
   {
     "revision": "ce2ec21ee5614a723d95d9fba856c216",
@@ -44,16 +40,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flash-cards-new/img/copy.7b016899.svg"
   },
   {
-    "revision": "47fb15ce6eb8bea622e648492898e1bb",
-    "url": "/flash-cards-new/img/down-arrow.47fb15ce.svg"
-  },
-  {
     "revision": "f9bfde579af5638f9c6ef5976a2949dd",
     "url": "/flash-cards-new/img/edit.f9bfde57.svg"
-  },
-  {
-    "revision": "77b21bd3a36a4de0a5f509a69a75bebb",
-    "url": "/flash-cards-new/img/export.77b21bd3.svg"
   },
   {
     "revision": "1b5abd49cab1d316dd9cf12e386134f0",
@@ -72,16 +60,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flash-cards-new/img/invisible.6d157714.svg"
   },
   {
-    "revision": "ba01ee5de623fa987260fc2903179bf6",
-    "url": "/flash-cards-new/img/left-arrow.ba01ee5d.svg"
-  },
-  {
     "revision": "aabd5f0b6076ab2b397479f030e60839",
     "url": "/flash-cards-new/img/link.aabd5f0b.svg"
   },
   {
-    "revision": "d508160b343e7639c197e4d59b3112d3",
-    "url": "/flash-cards-new/img/lock.d508160b.svg"
+    "revision": "8cc1df4694e4c5ff8c858dbc7ef1518e",
+    "url": "/flash-cards-new/img/lock.8cc1df46.svg"
   },
   {
     "revision": "845847597633a682710fd882aeb01425",
@@ -94,14 +78,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "04749ac10b918b412c39baa3b7d80444",
     "url": "/flash-cards-new/img/play-button.04749ac1.svg"
-  },
-  {
-    "revision": "52c0fe6786c15ab18fd241dae6cd3be9",
-    "url": "/flash-cards-new/img/play-button_disable.52c0fe67.svg"
-  },
-  {
-    "revision": "9d8aad6f8765a380016ad38c797fbbcc",
-    "url": "/flash-cards-new/img/plus.9d8aad6f.svg"
   },
   {
     "revision": "7753ba0867cf47b9e9525cc557641a99",
@@ -128,24 +104,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flash-cards-new/img/settings_folder.d9e03723.svg"
   },
   {
-    "revision": "b7992771334314da3d94626a3c00f216",
+    "revision": "7a29a9bb02b9c10259aa7449e19bec9a",
     "url": "/flash-cards-new/index.html"
   },
   {
-    "revision": "657279c467441f88b53a",
-    "url": "/flash-cards-new/js/app.8ec2ffc8.js"
+    "revision": "d50c56c377d09fc921c8",
+    "url": "/flash-cards-new/js/app.918a48bd.js"
   },
   {
     "revision": "d7924185ff9ab6cac739",
     "url": "/flash-cards-new/js/chunk-vendors.7650996d.js"
   },
   {
-    "revision": "cd006f61f4dcc4d77721",
-    "url": "/flash-cards-new/js/foldercontent.af88c986.js"
+    "revision": "b0d8749f5d1b548a1254",
+    "url": "/flash-cards-new/js/foldercontent.6eac9d62.js"
   },
   {
-    "revision": "e1da74122d2911c153ce",
-    "url": "/flash-cards-new/js/folders.d8f7b81a.js"
+    "revision": "c3bf966eee5c601de047",
+    "url": "/flash-cards-new/js/folders.feda26c9.js"
   },
   {
     "revision": "690de8e741af7dfa92fb",
@@ -156,16 +132,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flash-cards-new/js/login.b461aaae.js"
   },
   {
-    "revision": "ef49853c16bc60c3d468",
-    "url": "/flash-cards-new/js/profile.38f6a910.js"
+    "revision": "6707bc034a75c8783e28",
+    "url": "/flash-cards-new/js/profile.92cb487c.js"
   },
   {
     "revision": "5eced3a703fee9b100c0",
     "url": "/flash-cards-new/js/registration.f8fed151.js"
   },
   {
-    "revision": "32ce3d8408a73294a2ed",
-    "url": "/flash-cards-new/js/settings.ee3cf4ea.js"
+    "revision": "d9bdc5fa191d088b58a0",
+    "url": "/flash-cards-new/js/settings.952b20a5.js"
   },
   {
     "revision": "2656bc1b41460cffa6ded4078c2f4b24",
